9263d3212f4139dd9b919332cf0ab8d68f2f3c03
alyarosman2993-cmyk <alya.rosman2993@gmail.com>
2026-06-17 10:48:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "MAX bot is working"
+    return "MAX Photobank Bot работает"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print(data)
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     app.run()
