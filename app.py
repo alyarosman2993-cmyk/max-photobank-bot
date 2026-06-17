029971aa26b17a974bad4618c0722314d8033243
alyarosman2993-cmyk <alya.rosman2993@gmail.com>
2026-06-17 12:15:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, request
+import os
+import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+MAX_TOKEN = os.environ.get("MAX_TOKEN")
+MAX_API = "https://platform-api.max.ru"
+WEBHOOK_URL = "https://max-photobank-bot.onrender.com/webhook"
 
 @app.route("/")
 def home():
@@ -9,8 +15,53 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    print(data)
-    return {"status": "ok"}
+    print("MAX EVENT:", data, flush=True)
+    return jsonify({"status": "ok"})
+
+@app.route("/setup-webhook")
+def setup_webhook():
+    if not MAX_TOKEN:
+        return "Ошибка: MAX_TOKEN не найден в Render"
+
+    url = f"{MAX_API}/subscriptions"
+
+    payload = {
+        "url": WEBHOOK_URL,
+        "update_types": [
+            "message_created",
+            "bot_started"
+        ]
+    }
+
+    headers = {
+        "Authorization": MAX_TOKEN,
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    return {
+        "status_code": response.status_code,
+        "response": response.text
+    }
+
+@app.route("/check-webhook")
+def check_webhook():
+    if not MAX_TOKEN:
+        return "Ошибка: MAX_TOKEN не найден в Render"
+
+    url = f"{MAX_API}/subscriptions"
+
+    headers = {
+        "Authorization": MAX_TOKEN
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return {
+        "status_code": response.status_code,
+        "response": response.text
+    }
 
 if __name__ == "__main__":
     app.run()
